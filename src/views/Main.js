495bf915a8d766bedde5ef7cdc484e466bd85b3a
Nate Trichanh <natetrichanh@gmail.com>
2022-01-21 13:12:13 -0800

    initial commit

---
+++ src/views/Main.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import Form from '../components/Form';
+export default () => {
+    return (
+        <div>
+            <Form />
+        </div>
+    )
+}
+
